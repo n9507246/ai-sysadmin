2cd46e00f9b8d724d6217a7a28cb5fa24a5cb43b
Your Name <you@example.com>
2026-01-18 11:14:28 +0300

    бесконечный цикл для того что бы можно было вести диалог с моделью

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ import config
 
 def main():
 
-    user_input = "Когда родился Александр Сергеевич Пушкин?"
 
-    print(
-        YandexGPT().ask([
+    print("AI-ассистент запущен. Введите 'exit' для выхода.")
+    while True:
+        user_input = input("\nВы: ")
+        if user_input.lower() in ("exit", "quit"):
+            break
+
+        result = YandexGPT().ask([
             { "role": "system", "content": config.SYSTEM_PROMPT}, # установка роли системы(БЕЗ НЕГО МОДЕЛЬ НЕ РАБОТАЕТ)
             { "role": "user",  "content": user_input} # вопрос пользователя
-        ]))
+        ])
 
+        print(f"\nAI: {result}")
+
+       
 
 
 if __name__ == "__main__":
