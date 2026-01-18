06ee84e228392b51e3d1eb8e11e22ddf523fe59c
Your Name <you@example.com>
2026-01-18 11:06:23 +0300

    промпты вынесли в config.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from YandexGPT import YandexGPT
-
+import config 
 
 def main():
-   
-    SYSTEM_PROMPT = "You are a helpful assistant." 
+
     user_input = "Когда родился Александр Сергеевич Пушкин?"
 
     print(
         YandexGPT().ask([
-            { "role": "system", "content": SYSTEM_PROMPT}, # установка роли системы(БЕЗ НЕГО МОДЕЛЬ НЕ РАБОТАЕТ)
+            { "role": "system", "content": config.SYSTEM_PROMPT}, # установка роли системы(БЕЗ НЕГО МОДЕЛЬ НЕ РАБОТАЕТ)
             { "role": "user",  "content": user_input} # вопрос пользователя
         ]))
+
+
 
 if __name__ == "__main__":
     main()
